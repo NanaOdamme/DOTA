5ac0afe9c0bbee813cfbed34a16d502e34884d1f
NanaOdamme <eronastyles@gmail.com>
2024-05-15 11:43:39 +0000

    updated

--- src/First-page.jsx
+++ src/First-page.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom'; // Import Link from React Router
 const HomePage = () => {
   return (
     <div className="bg-cover h-screen flex items-center justify-center" style={{backgroundImage: "url('./assets/bg.gif')"}}>
       <div className="text-white text-center">
-        <h1 className="text-8xl font-bold mb-4">Welcome to Dota</h1>
-        <p className="text-lg">A place to buy and sell digital Assets</p>
-        <button className='text-white bg-teal-500 px-8 py-2 rounded mt-10 hover:bg-teal-200'>Get Started</button>
+        <h1 className="lg:text-8xl text-4xl md:text-6xl font-bold mb-5">Welcome to Dota</h1>
+        <p className="text-lg mb-10">A place to buy and sell digital Assets</p>
+        <Link to={'/home'} className='text-white bg-teal-500 px-8 py-2 rounded mt-10 hover:bg-teal-200'>Get Started</Link>
       </div>
     </div>
   );
