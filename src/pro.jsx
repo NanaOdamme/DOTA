b957479975d9e196adf06faddc8ec69b2f9dc09a
NanaOdamme <eronastyles@gmail.com>
2024-06-05 08:51:31 +0000

    updated the buttons in pro

--- src/pro.jsx
+++ src/pro.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import Footer from './Footer'
+import Footer from './Footer';
+
 const Pro = () => {
   return (
     <>
@@ -11,59 +12,60 @@ const Pro = () => {
           </p>
         </div>
         <div className="flex justify-center mb-10">
-        <div className="grid grid-cols-1 lg:grid-cols-3 py-5 px-10 gap-8">
-          <div className=" bg-sky-300 py-5 px-8 rounded-lg shadow-lg" style={{width:280}}>
-            <h5 className="font-bold mb-5">Basic</h5>
-            <h1 className="text-5xl font-bold mb-5">Free</h1> 
-            <p className="font-bold mb-5">Get started with the basic plan</p>
-            <ul className="mb-16">
-              <li className="mb-1"><i class="bi bi-check-circle-fill"></i> 1 User</li>
-              <li className="mb-1"><i class="bi bi-check-circle-fill"></i> 2 Projects</li>
-              <li className="mb-1"><i class="bi bi-check-circle-fill"></i> 5 GB Storage</li>
-            </ul>
-            <div className="flex justify-center ">
-          <button className="bg-zinc-900 rounded-lg shadow-lg text-white py-3 px-10 w-56">Start Free Plan</button>
+          <div className="grid grid-cols-1 lg:grid-cols-3 py-5 px-10 gap-8">
+            <div className="bg-sky-300 py-5 px-8 rounded-lg shadow-lg flex flex-col justify-between" style={{ width: 280 }}>
+              <div>
+                <h5 className="font-bold mb-5">Basic</h5>
+                <h1 className="text-5xl font-bold mb-5">Free</h1>
+                <p className="font-bold mb-5">Get started with the basic plan</p>
+                <ul className="mb-5">
+                  <li className="mb-1"><i className="bi bi-check-circle-fill"></i> 1 User</li>
+                  <li className="mb-1"><i className="bi bi-check-circle-fill"></i> 2 Projects</li>
+                  <li className="mb-1"><i className="bi bi-check-circle-fill"></i> 5 GB Storage</li>
+                </ul>
+              </div>
+              <div className="flex justify-center">
+                <button className="bg-zinc-900 rounded-lg shadow-lg text-white py-3 px-10 w-56">Start Free Plan</button>
+              </div>
+            </div>
+            <div className="bg-yellow-400 py-5 px-8 rounded-lg shadow-lg flex flex-col justify-between" style={{ width: 280 }}>
+              <div>
+                <h5 className="font-bold mb-5">Basic</h5>
+                <h1 className="text-5xl font-bold mb-5">Free</h1>
+                <p className="font-bold mb-5">Get started with the basic plan</p>
+                <ul className="mb-5">
+                  <li className="mb-1"><i className="bi bi-check-circle-fill"></i> 1 User</li>
+                  <li className="mb-1"><i className="bi bi-check-circle-fill"></i> 2 Projects</li>
+                  <li className="mb-1"><i className="bi bi-check-circle-fill"></i> 5 GB Storage</li>
+                  <li className="mb-1"><i className="bi bi-check-circle-fill"></i> 2 Projects</li>
+                  <li className="mb-1"><i className="bi bi-check-circle-fill"></i> 5 GB Storage</li>
+                </ul>
+              </div>
+              <div className="flex justify-center">
+                <button className="bg-zinc-900 rounded-lg shadow-lg text-white py-3 px-10 w-56">Start Free Plan</button>
+              </div>
+            </div>
+            <div className="bg-rose-400 py-5 px-8 rounded-lg shadow-lg flex flex-col justify-between" style={{ width: 280 }}>
+              <div>
+                <h5 className="font-bold mb-5">Basic</h5>
+                <h1 className="text-5xl font-bold mb-5">Free</h1>
+                <p className="font-bold mb-5">Get started with the basic plan</p>
+                <ul className="mb-5">
+                  <li className="mb-1"><i className="bi bi-check-circle-fill"></i> 1 User</li>
+                  <li className="mb-1"><i className="bi bi-check-circle-fill"></i> 2 Projects</li>
+                  <li className="mb-1"><i className="bi bi-check-circle-fill"></i> 5 GB Storage</li>
+                </ul>
+              </div>
+              <div className="flex justify-center">
+                <button className="bg-zinc-900 rounded-lg shadow-lg text-white py-3 px-10 w-56">Start Free Plan</button>
+              </div>
+            </div>
           </div>
-          
-          </div>
-          <div className=" bg-yellow-400 py-5 px-8 rounded-lg shadow-lg" style={{width:280}}>
-            <h5 className="font-bold mb-5">Basic</h5>
-            <h1 className="text-5xl font-bold mb-5">Free</h1>
-            <p className="font-bold mb-5">Get started with the basic plan</p>
-            <ul className="mb-16">
-              <li className="mb-1"><i class="bi bi-check-circle-fill"></i> 1 User</li>
-              <li className="mb-1"><i class="bi bi-check-circle-fill"></i> 2 Projects</li>
-              <li className="mb-1"><i class="bi bi-check-circle-fill"></i> 5 GB Storage</li>
-              <li className="mb-1"><i class="bi bi-check-circle-fill"></i> 2 Projects</li>
-              <li className="mb-1"><i class="bi bi-check-circle-fill"></i> 5 GB Storage</li>
-            </ul>
-            <div className="flex justify-center">
-          <button className="bg-zinc-900 rounded-lg shadow-lg text-white py-3 px-10 w-96">Start Free Plan</button>
-          </div>
-          
-          </div>
-          <div className=" bg-rose-400 py-5 px-8 rounded-lg shadow-lg" style={{width:280}}>
-            <h5 className="font-bold mb-5">Basic</h5>
-            <h1 className="text-5xl font-bold mb-5">Free</h1>
-            <p className="font-bold mb-5">Get started with the basic plan</p>
-            <ul className="mb-16">
-              <li className="mb-1"><i class="bi bi-check-circle-fill"></i> 1 User</li>
-              <li className="mb-1"><i class="bi bi-check-circle-fill"></i> 2 Projects</li>
-              <li className="mb-1"><i class="bi bi-check-circle-fill"></i> 5 GB Storage</li>
-            </ul>
-            <div className="flex justify-center">
-          <button className="bg-zinc-900 rounded-lg shadow-lg text-white py-3 px-10 w-96">Start Free Plan</button>
-          </div>
-          
-          </div>
-
-          
-          
-        </div>
         </div>
         <Footer />
       </div>
     </>
   );
 };
+
 export default Pro;
