8e37a8b0343044c97bc535455dc21ba2b3d4a62d
KingBygone0 <kingbygone@icloud.com>
2024-06-05 14:24:19 +0000

    passed test

--- src/pro.test.jsx
+++ src/pro.test.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Pro from '../src/pro'; // Adjust the path as necessary
+import Pro from './pro.jsx'; // Adjust the path as necessary
 
-test('renders Pro  with correct content', () => {
+test('renders Pro component with correct content', () => {
     render(
         <Router>
             <Pro />
@@ -16,9 +16,9 @@ test('renders Pro  with correct content', () => {
     expect(screen.getByText(/Subscription Plans/i)).toBeInTheDocument();
 
     // Check if the plan titles are rendered
-    expect(screen.getByText(/Basic/i)).toBeInTheDocument();
-    expect(screen.getByText(/Advanced/i)).toBeInTheDocument();
-    expect(screen.getByText(/Pro/i)).toBeInTheDocument();
+    expect(screen.getAllByText(/Basic/i)[0]).toBeInTheDocument();
+    expect(screen.getAllByText(/Advanced/i)[0]).toBeInTheDocument();
+    expect(screen.getAllByText(/Pro/i)[0]).toBeInTheDocument();
 
     // Check if the plan descriptions are rendered
     expect(screen.getByText(/Get started with Basic Plan/i)).toBeInTheDocument();
