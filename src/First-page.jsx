9b20e4e5954ffd2574c083ea2260ef6d1a82f72e
NanaOdamme <eronastyles@gmail.com>
2024-06-07 16:15:48 +0000

    updated

--- src/First-page.jsx
+++ src/First-page.jsx
@@ -6,7 +6,7 @@ const HomePage = () => {
       <div className="text-white text-center">
         <h1 className="lg:text-8xl text-4xl md:text-6xl font-bold mb-5">Welcome to Dota</h1>
         <p className="text-lg mb-10">A place to buy and sell digital Assets</p>
-        <Link to={'/login'} className='text-white bg-teal-500 px-8 py-2 rounded mt-10 hover:bg-teal-200'>Get Started</Link>
+        <Link to={'/home'} className='text-white bg-teal-500 px-8 py-2 rounded mt-10 hover:bg-teal-200'>Get Started</Link>
       </div>
     </div>
   );
